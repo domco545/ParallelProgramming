8352bca6c5bdb85d90ee509343b18a13f881fbfb
domco545 <43946320+domco545@users.noreply.github.com>
2022-10-04 23:38:36 +0200

    Create ui

--- ParallelProgramming.Gui/ViewModels/MainWindowViewModel.cs
+++ ParallelProgramming.Gui/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,23 @@
-﻿namespace ParallelProgramming.Gui.ViewModels
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using DynamicData;
+using ReactiveUI;
+
+namespace ParallelProgramming.Gui.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        private ObservableCollection<long> _primes = new ObservableCollection<long>();
+        public ObservableCollection<long> Primes
+        {
+            get => _primes;
+            set => this.RaiseAndSetIfChanged(ref _primes, value);
+        }
+
+        public void OnClick_CalculateButton()
+        {
+            Primes.AddRange(new List<long>(){100,1000000,1000000000});
+        }
     }
 }
