7b0b0198f3afb2a643dd73b2806515fb39598c85
Radoslav Backovsky <radoslav.backovsky@gmail.com>
2022-10-05 21:31:36 +0200

    no message

--- ParallelProgramming.Gui/ViewModels/MainWindowViewModel.cs
+++ ParallelProgramming.Gui/ViewModels/MainWindowViewModel.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Threading.Tasks;
 using Avalonia.Controls;
+using Avalonia.Media;
 using DynamicData;
+using ParallelProgramming.Core;
 using ReactiveUI;
 
 namespace ParallelProgramming.Gui.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        // logic instance
+        private readonly Primes _primesLogic;
+
+        public MainWindowViewModel()
+        {
+            _primesLogic = new Primes();
+        }
+
         private ComboBoxItem _selectedAlgorithm;
         public ComboBoxItem SelectedAlgorithm
         {
@@ -20,6 +31,21 @@ namespace ParallelProgramming.Gui.ViewModels
                 this.RaiseAndSetIfChanged(ref _selectedAlgorithm, value);
             }
         }
+
+        private int _fromInputField;
+        public int FromInputField
+        {
+            get => _fromInputField;
+            set => this.RaiseAndSetIfChanged(ref _fromInputField, value);
+        }
+        
+        private int _toInputField;
+        public int ToInputField
+        {
+            get => _toInputField;
+            set => this.RaiseAndSetIfChanged(ref _toInputField, value);
+        }
+        
         private ObservableCollection<long> _primes = new ObservableCollection<long>();
         public ObservableCollection<long> Primes
         {
@@ -29,9 +55,16 @@ namespace ParallelProgramming.Gui.ViewModels
 
         public async Task OnClick_CalculateButton()
         {
-            await Task.Delay(1000);
-            Primes.AddRange(new List<long>(){100,1000000,1000000000});
-            Console.WriteLine(_selectedAlgorithm.Content);
+            await Task.Delay(100);
+            // _primesLogic.GetPrimesSequential()
+
+            // Primes.AddRange(new List<long>(){100,1000000,1000000000});
+            // Console.WriteLine(_selectedAlgorithm.Content);
+            Console.WriteLine("from: " + _fromInputField);
+            Console.WriteLine("to: " + _toInputField);
+
+            Console.WriteLine("click");
+
         }
     }
 }
